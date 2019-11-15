6c6801039571ccbc8d3c6720828eff641c03d5d2
Adrien Poupard <adrien@smartb.city>
2019-11-15 10:56:25 +0100

    Add Stepper class in index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import SBDrawerDetails from "./layout/drawerDetails";
 import SBDrawerMenu from "./layout/drawermenu";
 import SBProfile from "./layout/profile"
 import SBApp from "./layout/sbapp";
-import SBStepper from "./layout/sbstepper";
+import SBStepper, {OnNextHandles, StepDetails} from "./layout/sbstepper";
 
-export {SBAppBar, SBDrawerDetails, SBDrawerMenu, SBProfile, SBApp, SBStepper}
+export {SBAppBar, SBDrawerDetails, SBDrawerMenu, SBProfile, SBApp, SBStepper, OnNextHandles, StepDetails}
