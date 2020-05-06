e51048ecef2a36bdbd8b2cf3c0766809ddf8c761
Adrien Poupard <adrien@smartb.city>
2020-05-06 09:05:11 +0200

    Migrate storybook to beta version

--- packages/components/src/stories/Demo/Card.stories.tsx
+++ packages/components/src/stories/Demo/Card.stories.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import Card, { Direction} from '../../Card';
+import Card, {Direction} from '../../Card';
 import {withKnobs, text, select, number} from "@storybook/addon-knobs";
 import {Typography} from '@material-ui/core';
-import { createStyles, makeStyles} from '@material-ui/core/styles';
-// @ts-ignore
-import { Title, Subtitle, Description, Primary, Props, Stories } from '@storybook/addon-docs/blocks';
+import {createStyles, makeStyles} from '@material-ui/core/styles';
+import {Title, Subtitle, Description, Primary, Props} from '@storybook/addon-docs/blocks';
+import ButtonTS from "../../ButtonTest";
 
 
 const useStyles = makeStyles(() =>
   createStyles({
     text: {
-        textAlign:"center",
+      textAlign: "center",
     },
     card: {
-      margin:"auto",
-      maxWidth:"500px"
+      margin: "auto",
+      maxWidth: "500px"
     }
   }),
 );
@@ -24,27 +24,30 @@ export default {
   decorators: [withKnobs],
   parameters: {
     docs: {
+
       page: () => (
         <>
           <Title>Custom Demo Card</Title>
-          <Title />
           <Subtitle>With full parameters</Subtitle>
-          <Description />
-          <Description></Description>
-          <Primary />
+          <Description>Description</Description>
+          <Primary/>
           <Props
             components={{
               'card': Card,
               'cardSimplified': Card
             }}
           />
-          <Stories title="Stories title" />
+          <ButtonTS onClick={() => {
+          }}/>
+          {/*<Stories title="Stories title">*/}
+          {/*  <ButtonTS onClick={() => {*/}
+          {/*  }} color={"blue"}/>*/}
+          {/*</Stories>*/}
         </>
       ),
     },
   }
 };
-
 
 
 export const card = () => {
@@ -54,16 +57,17 @@ export const card = () => {
 
   const nbOfChildren = number("number of children", 2);
   const children: React.ReactNode[] = [];
-  for (var i = 0; i < nbOfChildren; i++){
+  for (var i = 0; i < nbOfChildren; i++) {
     children.push(<Typography className={classes.text} key={i} variant="body2" color="textSecondary" component="p">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
     </Typography>)
   }
 
   const dividerText = text("dividerText", "Or");
 
-  const dividerDirection = select("dividerDirection",{Horizontal: 'horizontal',Vertical: 'vertical',}, 'horizontal');
+  const dividerDirection = select("dividerDirection", {Horizontal: 'horizontal', Vertical: 'vertical',}, 'horizontal');
 
   const footerString = text("footer", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
 
@@ -71,27 +75,28 @@ export const card = () => {
     {footerString}
   </Typography>;
 
-  const logo = select("logo",{Default: 'default',Document: 'document'}, 'default');
+  const logo = select("logo", {Default: 'default', Document: 'document'}, 'default');
 
   return (<Card
-          className={classes.card}
-          header={header}
-          dividerText={dividerText}
-          dividerDirection={dividerDirection as Direction}
-          footer={footer}
-          logo={logo}>
-            {children}
-          </Card>)
+    className={classes.card}
+    header={header}
+    dividerText={dividerText}
+    dividerDirection={dividerDirection as Direction}
+    footer={footer}
+    logo={logo}>
+    {children}
+  </Card>)
 }
 
 export const cardSimplified = () => {
   const classes = useStyles();
 
   return (<Card
-          className={classes.card} >
-            <Typography className={classes.text} variant="body2" color="textSecondary" component="p">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </Typography>
-          </Card>)
+    className={classes.card}>
+    <Typography className={classes.text} variant="body2" color="textSecondary" component="p">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </Typography>
+  </Card>)
 }
