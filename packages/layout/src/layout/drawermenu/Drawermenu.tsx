ebd88b469bcb2255da4ed3a02c2eddd2f0edbb01
basile-savouret <47100280+basile-savouret@users.noreply.github.com>
2020-05-28 15:46:12 +0200

    SBApp improved

--- packages/layout/src/layout/drawermenu/Drawermenu.tsx
+++ packages/layout/src/layout/drawermenu/Drawermenu.tsx
@@ -1,58 +1,77 @@
-import React from "react";
-import styled from "styled-components";
-import MuiDrawer from "@material-ui/core/Drawer";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import {MenuItem} from "./menu";
-import StyleProps from "../StyleProps";
-import {IconProfileProps} from "../profile";
-import { PanelProfile } from "../PanelProfile";
+import React from 'react'
+import styled from 'styled-components'
+import MuiDrawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import { MenuItem } from './menu'
+import StyleProps from '../StyleProps'
+import { IconProfileProps } from '../profile'
+import { PanelProfile } from '../PanelProfile'
 
 // TODO style only on desktop size
 const Nav = styled.nav<WidthProps>`
-  width: ${props => props.width};
+  width: ${(props) => props.width};
   flex-shrink: 0;
-`;
+`
 
 export interface WidthProps {
-  width: number;
+  width: number
 }
 
 interface Props {
-  className?: string;
-  open: boolean;
-  menu?: MenuItem[];
-  styleProps: StyleProps;
-  children?:React.ReactNode;
-  profilesProps?:IconProfileProps[];
-  navBarContent?:React.ReactNode;
+  className?: string
+  open: boolean
+  menu?: MenuItem[]
+  styleProps: StyleProps
+  children?: React.ReactNode
+  profilesProps?: IconProfileProps[]
+  navBarContent?: React.ReactNode
 }
 
-export const SBDrawerMenu = ({open, className, menu, styleProps, children, profilesProps, navBarContent}: Props) => {
+export const SBDrawerMenu = ({
+  open,
+  className,
+  menu,
+  styleProps,
+  children,
+  profilesProps,
+  navBarContent
+}: Props) => {
   return (
     <Nav width={styleProps.menuWidth}>
       <MuiDrawer
-        variant={"persistent"}
+        variant={'persistent'}
         open={open || undefined}
-        className={className}>
-        <div>
-          <List>
-            {menu && menu.map(item => (
-              <ListItem button key={item.key} onClick={() => item.goto && item.goto()}>
-                <ListItemIcon>{item.icon}</ListItemIcon>
-                <ListItemText primary={item.label} />
-              </ListItem>
-            ))}
-          </List>
-        </div>
+        className={className}
+      >
+        {menu && (
+          <div>
+            <List>
+              {menu.map((item) => (
+                <ListItem
+                  button
+                  key={item.key}
+                  onClick={() => item.goto && item.goto()}
+                >
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.label} />
+                </ListItem>
+              ))}
+            </List>
+          </div>
+        )}
         {children}
         {navBarContent}
-        {profilesProps && profilesProps.map(profileProps => (
-          <PanelProfile menu={profileProps.menu} key={profileProps.menu.key}/>
-        ))}
+        {profilesProps &&
+          profilesProps.map((profileProps) => (
+            <PanelProfile
+              menu={profileProps.menu}
+              key={profileProps.menu.key}
+            />
+          ))}
       </MuiDrawer>
     </Nav>
-  );
-};
+  )
+}
