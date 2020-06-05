a8e5814dd671fa669203a910d4adf43e5191c9f8
basile-savouret <47100280+basile-savouret@users.noreply.github.com>
2020-06-05 14:44:12 +0200

    corrected a problem on the stepper

--- packages/layout/src/layout/Stepper/StepperImg.tsx
+++ packages/layout/src/layout/Stepper/StepperImg.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Theme, Hidden } from '@material-ui/core'
+import { Grid, Hidden } from '@material-ui/core'
 import makeStyles from '@material-ui/core/styles/makeStyles'
 import createStyles from '@material-ui/core/styles/createStyles'
 import clsx from 'clsx'
