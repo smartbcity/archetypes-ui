c54f5b51ce69f78aabc9b1d0c597c69548a92e30
basile-savouret <47100280+basile-savouret@users.noreply.github.com>
2020-06-08 16:45:22 +0200

    fixes for the integration in moblite.eco

--- packages/components/src/ThemeContextProvider/ThemeContextProvider.tsx
+++ packages/components/src/ThemeContextProvider/ThemeContextProvider.tsx
@@ -1,4 +1,5 @@
 import React, { createContext } from 'react'
+import { createMuiTheme, ThemeProvider, Theme as MuiTheme } from '@material-ui/core'
 
 export interface Theme {
   primaryColor: string
@@ -7,14 +8,64 @@ export interface Theme {
   shadows: string[]
 }
 
+const muiTheme = (theme: Theme, customMuiTheme?: MuiTheme) => {
+const themeOverride = {
+  overrides: {
+    MuiPaper: {
+      elevation1: {
+        boxShadow: theme.shadows[1]
+      },
+      elevation2: {
+        boxShadow: theme.shadows[2]
+      },
+      elevation3: {
+        boxShadow: theme.shadows[3]
+      },
+      elevation4: {
+        boxShadow: theme.shadows[4]
+      },
+      elevation5: {
+        boxShadow: theme.shadows[5]
+      },
+      elevation6: {
+        boxShadow: theme.shadows[6]
+      },
+      elevation7: {
+        boxShadow: theme.shadows[7]
+      },
+      elevation8: {
+        boxShadow: theme.shadows[8]
+      },
+      elevation9: {
+        boxShadow: theme.shadows[9]
+      },
+      elevation10: {
+        boxShadow: theme.shadows[10]
+      },
+      elevation11: {
+        boxShadow: theme.shadows[11]
+      },
+      elevation12: {
+        boxShadow: theme.shadows[12]
+      }
+    }
+  }
+}
+return customMuiTheme ?  createMuiTheme(themeOverride, customMuiTheme) : createMuiTheme(themeOverride)};
+
 export const themeContext = createContext({} as Theme)
 
 interface ThemeContextProviderProps {
-  theme: Theme
-  children: React.ReactNode
+  theme: Theme;
+  children: React.ReactNode;
+  customMuiTheme?: MuiTheme;
 }
 
 export const ThemeContextProvider = (props: ThemeContextProviderProps) => {
-  const { theme, children } = props
-  return <themeContext.Provider value={theme}>{children}</themeContext.Provider>
+  const { theme, children, customMuiTheme } = props;
+  const defaultMuiTheme = muiTheme(theme, customMuiTheme);
+  
+  return <themeContext.Provider value={theme}>
+          <ThemeProvider theme={defaultMuiTheme}>{children}</ThemeProvider>
+        </themeContext.Provider>
 }
--- packages/storybook/src/stories/Components/DatePicker.stories.tsx
+++ packages/storybook/src/stories/Components/DatePicker.stories.tsx
@@ -6,13 +6,24 @@ import {
 } from '@smartb/archetypes-ui-components'
 import { withA11y } from '@storybook/addon-a11y'
 import { myTheme } from '../../Docs/Theme/Theme'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
 
 export default {
   title: 'Components|DatePicker',
   decorators: [withKnobs, withA11y]
 }
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    color:{
+      color:"blue"
+    }
+  })
+)
+
+
 export const DatePicker = () => {
+  const classes = useStyles();
   const [date, setDate] = useState('')
   const label = text('label', 'Start')
   return (
@@ -21,6 +32,7 @@ export const DatePicker = () => {
         value={date}
         label={label}
         onChange={(date: string) => setDate(date)}
+        inputClassName={classes.color}
       />
     </ThemeContextProvider>
   )
