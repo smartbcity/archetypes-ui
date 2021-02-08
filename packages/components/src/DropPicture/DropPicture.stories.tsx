c7c89ada1ffc6301ba8c61dcaed41736630397c8
teddy <teddy@smartb.city>
2021-02-08 14:48:03 +0100

    add drop picture story and comments (missing comment on src prop)

---
+++ packages/components/src/DropPicture/DropPicture.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { DropPicture as AruiDropPicture, DropPictureProps } from './DropPicture'
+import { Meta } from '@storybook/react'
+import { Story } from '@storybook/react/types-6-0'
+
+export default {
+  title: 'Components/DropPicture',
+  component: AruiDropPicture
+} as Meta
+
+const Template: Story<DropPictureProps> = (args: DropPictureProps) => (
+  <AruiDropPicture {...args}></AruiDropPicture>
+)
+
+export const DropPicture = Template.bind({})
+DropPicture.args = {
+  onRemovePicture: () => {},
+  onPictureDroped: () => {}
+}
