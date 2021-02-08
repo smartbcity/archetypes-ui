1d02b83749ec9df8f854996a68c3923e8c43b905
teddy <teddy@smartb.city>
2021-02-08 12:16:27 +0100

    add checkbox props comment and story

---
+++ packages/components/src/Checkbox/CheckBox.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { CheckBox as AruiCheckBox, CheckBoxProps } from './CheckBox'
+import { Meta } from '@storybook/react'
+import { Story } from '@storybook/react/types-6-0'
+
+export default {
+  title: 'Components/CheckBox',
+  component: AruiCheckBox
+} as Meta
+
+const Template: Story<CheckBoxProps> = (args: CheckBoxProps) => (
+  <AruiCheckBox {...args} />
+)
+
+export const CheckBox = Template.bind({})
+CheckBox.args = {
+  checked: false
+}
