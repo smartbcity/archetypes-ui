1a60c840adc8a316f1a410f25ad913082de40254
Adrien Poupard <adrien@smartb.city>
2021-02-11 19:48:48 +0100

    FIX-21 - Add smartb icon

---
+++ .storybook/SmartBTheme.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+import logo from "../docs/public/smartb.png";
+
+export default create({
+  base: "light",
+  brandTitle: "SmartB F2",
+  brandUrl: "https://docs.smartb.city/f2",
+  brandImage: logo,
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/addons";
+import SmartBTheme from "./SmartBTheme";
+
+addons.setConfig({
+  theme: SmartBTheme,
+});
