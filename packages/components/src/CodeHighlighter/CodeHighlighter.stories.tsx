b32bc64dce17a8b28e5d8e32a2bd35501de4f1bf
Adrien Poupard <adrien@smartb.city>
2021-04-19 16:36:06 +0200

    folders are case sensitive

--- packages/components/src/CodeHighlighter/CodeHighlighter.stories.tsx
+++ packages/components/src/CodeHighlighter/CodeHighlighter.stories.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
-import { CodeHighlighter as AruiCodeHighlighter, CodeHighlighterProps } from './CodeHighlighter'
+import {
+  CodeHighlighter as AruiCodeHighlighter,
+  CodeHighlighterProps
+} from './CodeHighlighter'
 import { Meta } from '@storybook/react'
 import { Story } from '@storybook/react/types-6-0'
 
 export default {
-    title: 'Components/CodeHighlighter',
-    component: AruiCodeHighlighter
+  title: 'Components/CodeHighlighter',
+  component: AruiCodeHighlighter
 } as Meta
 
 const Template: Story<CodeHighlighterProps> = (args: CodeHighlighterProps) => (
-    <AruiCodeHighlighter {...args} />
+  <AruiCodeHighlighter {...args} />
 )
 
 export const CodeHighlighter = Template.bind({})
 CodeHighlighter.args = {
-    object: { arg1: "test", arg2: 1 },
-    language: "typescript"
+  object: { arg1: 'test', arg2: 1 },
+  language: 'typescript'
 }
 CodeHighlighter.storyName = 'CodeHighlighter'
--- packages/components/src/CodeHighlighter/CodeHighlighter.tsx
+++ packages/components/src/CodeHighlighter/CodeHighlighter.tsx
@@ -2,27 +2,41 @@ import React from 'react'
 import SyntaxHighlighter, {
   SyntaxHighlighterProps
 } from 'react-syntax-highlighter'
-import { atomOneDark, androidstudio, atomOneLight, darcula, gruvboxLight, hopscotch, tomorrowNight, tomorrow } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import {
+  atomOneDark,
+  androidstudio,
+  atomOneLight,
+  darcula,
+  gruvboxLight,
+  hopscotch,
+  tomorrowNight,
+  tomorrow
+} from 'react-syntax-highlighter/dist/esm/styles/hljs'
 import stringifyObject from 'stringify-object'
 import { useMemo } from 'react'
-import dedent from 'dedent';
+import dedent from 'dedent'
 
-export type darkStyles = "atomOneDark" | "androidstudio" | "darcula" | "hopscotch" | "tomorrowNight"
-export type lightStyles = "atomOneLight" | "gruvboxLight" | "tomorrow"
+export type darkStyles =
+  | 'atomOneDark'
+  | 'androidstudio'
+  | 'darcula'
+  | 'hopscotch'
+  | 'tomorrowNight'
+export type lightStyles = 'atomOneLight' | 'gruvboxLight' | 'tomorrow'
 
-const highlightStyleMap = new Map<(darkStyles | lightStyles), any>([
-  ["atomOneDark", atomOneDark],
-  ["androidstudio", androidstudio],
-  ["atomOneLight", atomOneLight],
-  ["darcula", darcula],
-  ["gruvboxLight", gruvboxLight],
-  ["hopscotch", hopscotch],
-  ["tomorrowNight", tomorrowNight],
-  ["tomorrow", tomorrow],
-]);
+const highlightStyleMap = new Map<darkStyles | lightStyles, any>([
+  ['atomOneDark', atomOneDark],
+  ['androidstudio', androidstudio],
+  ['atomOneLight', atomOneLight],
+  ['darcula', darcula],
+  ['gruvboxLight', gruvboxLight],
+  ['hopscotch', hopscotch],
+  ['tomorrowNight', tomorrowNight],
+  ['tomorrow', tomorrow]
+])
 
 export interface CodeHighlighterProps extends SyntaxHighlighterProps {
-    /**
+  /**
    * The code to highlight
    */
   code?: string
@@ -30,17 +44,17 @@ export interface CodeHighlighterProps extends SyntaxHighlighterProps {
    * The javascript object to highlight. The prop `code` has the display priority. The object is automaticaly stringified and formated properly
    */
   object?: Object
-    /**
+  /**
    * If true, the code passed in the prop `code` will be indented properly
    * @default false
    */
-  indentCode?:boolean
-   /**
+  indentCode?: boolean
+  /**
    * the language of the code
    * @default "typescript"
    */
   language?: string
-   /**
+  /**
    * the stye of the highlight
    * @default "atomOneDark"
    */
@@ -48,13 +62,20 @@ export interface CodeHighlighterProps extends SyntaxHighlighterProps {
 }
 
 export const CodeHighlighter = (props: CodeHighlighterProps) => {
-  const { code, children, indentCode = false, object, style = "atomOneDark", ...other } = props
+  const {
+    code,
+    children,
+    indentCode = false,
+    object,
+    style = 'atomOneDark',
+    ...other
+  } = props
   const formatedObject = useMemo(() => {
     if (!object) return
-      return stringifyObject(object, {
-        indent: '  ',
-        singleQuotes: false
-      })
+    return stringifyObject(object, {
+      indent: '  ',
+      singleQuotes: false
+    })
   }, [object])
 
   const indentedCode = useMemo(() => {
@@ -68,7 +89,7 @@ export const CodeHighlighter = (props: CodeHighlighterProps) => {
   const selectedStyle = useMemo(() => highlightStyleMap.get(style), [style])
 
   return (
-    <SyntaxHighlighter language="typescript" {...other} style={selectedStyle}>
+    <SyntaxHighlighter language='typescript' {...other} style={selectedStyle}>
       {indentedCode ?? code ?? formatedObject ?? children}
     </SyntaxHighlighter>
   )
