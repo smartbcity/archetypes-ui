ffdcafcd258f9b563d00c6d4106260b44ae2b3b9
Basile Savouret <basile@smartb.city>
2021-04-20 11:31:10 +0200

    fixed dependencies and version 0.1.28

--- globalRollup.config.js
+++ globalRollup.config.js
@@ -6,15 +6,10 @@ import image from "@rollup/plugin-image";
 import json from "rollup-plugin-json";
 import svgr from "@svgr/rollup";
 
-const globalPackageJson = require("../../package.json");
-
 const getGlobal = (localPackageJson) => {
   const externalsDependencies = Object.keys(
-    globalPackageJson.dependencies || {}
-  )
-    .concat(Object.keys(globalPackageJson.peerDependencies || {}))
-    .concat(Object.keys(localPackageJson.dependencies || {}))
-    .concat(Object.keys(localPackageJson.peerDependencies || {}));
+    localPackageJson.dependencies || {}
+  ).concat(Object.keys(localPackageJson.peerDependencies || {}));
   return {
     external: externalsDependencies,
     output: [
