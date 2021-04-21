77ac59c93bc8f82875a9dd8595d21b905b751063
Basile Savouret <basile@smartb.city>
2021-04-21 13:27:33 +0200

    tooltip component

---
+++ packages/components/src/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import { Tooltip as AruiTooltip, TooltipBasicProps } from './Tooltip'
+import { Meta } from '@storybook/react'
+import { Story } from '@storybook/react/types-6-0'
+import { Button } from '../Buttons'
+
+export default {
+  title: 'Components/Tooltip',
+  component: AruiTooltip
+} as Meta
+
+const Template: Story<TooltipBasicProps> = (args: TooltipBasicProps) => {
+  const { open, ...other } = args
+  return (
+    <AruiTooltip {...other} open={open ? true : undefined}>
+      <Button>Element to tooltiped</Button>
+    </AruiTooltip>
+  )
+}
+
+export const Tooltip = Template.bind({})
+
+Tooltip.args = {
+  helperText: 'A helper message'
+}
+
+Tooltip.storyName = 'Tooltip'
---
+++ packages/components/src/Tooltip/Tooltip.tsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import {
+  Tooltip as MuiTooltip,
+  TooltipProps as MuiTooltipProps
+} from '@material-ui/core'
+import {
+  MergeMuiElementProps,
+  BasicProps,
+  lowLevelStyles,
+  Theme,
+  useTheme
+} from '@smartb/archetypes-ui-themes'
+
+const useStyles = (theme: Theme) =>
+  lowLevelStyles({
+    root: {
+      background: theme.primaryColor,
+      fontSize: '13px',
+      padding: '8px'
+    },
+    arrow: {
+      color: theme.primaryColor,
+      width: '16px !important',
+      height: '12px !important',
+      marginTop: '-11px !important',
+      '&::before': {
+        borderRadius: '2px'
+      }
+    }
+  })
+
+export interface TooltipBasicProps extends BasicProps {
+  /**
+   * The element to tooltiped
+   */
+  children: React.ReactElement<any, any>
+  /**
+   * The text that will be displayed in the tooltip
+   */
+  helperText: string
+  /**
+   * Indicates wether the tooltip is open or not. If open is undefined the openning of the tooltip
+   * will be actionned by the hover on the given element. You can use the Material-ui `onClose` and `onOpen` props to control
+   * the tooltip openning manually.
+   */
+  open?: boolean
+}
+
+export type TooltipProps = MergeMuiElementProps<
+  Omit<MuiTooltipProps, 'title'>,
+  TooltipBasicProps
+>
+
+export const Tooltip = (props: TooltipProps) => {
+  const {
+    children,
+    helperText,
+    style,
+    className,
+    id,
+    classes,
+    open,
+    ...other
+  } = props
+  const theme = useTheme()
+  const defaultClasses = useStyles(theme)()
+  return (
+    <MuiTooltip
+      id={id}
+      className={className}
+      style={style}
+      arrow
+      classes={{
+        ...classes,
+        tooltip: defaultClasses.root,
+        arrow: defaultClasses.arrow
+      }}
+      {...other}
+      open={open}
+      title={helperText}
+      placement='bottom'
+    >
+      {children}
+    </MuiTooltip>
+  )
+}
