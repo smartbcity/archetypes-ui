cc0eeb0eebc0b0457eccbcdc5d47221f3fbd5db0
Basile Savouret <basile@smartb.city>
2021-04-30 09:55:26 +0200

    rollup config update + v0.1.33

--- globalRollup.config.js
+++ globalRollup.config.js
@@ -4,6 +4,7 @@ import typescript from "rollup-plugin-typescript2";
 import image from "@rollup/plugin-image";
 import json from "rollup-plugin-json";
 import svgr from "@svgr/rollup";
+import nodeGlobals from 'rollup-plugin-node-globals';
 
 const getGlobal = (localPackageJson) => {
   const externalsDependencies = Object.keys(
@@ -11,6 +12,10 @@ const getGlobal = (localPackageJson) => {
   ).concat(Object.keys(localPackageJson.peerDependencies || {}));
   return {
     external: externalsDependencies,
+    globals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+    },
     output: [
       {
         file: localPackageJson.main,
@@ -19,7 +24,7 @@ const getGlobal = (localPackageJson) => {
       },
       {
         file: localPackageJson.module,
-        format: "es",
+        format: "esm",
         sourcemap: true,
       },
     ],
@@ -27,13 +32,15 @@ const getGlobal = (localPackageJson) => {
       resolve({
         browser: true,
         preferBuiltins: false,
-        extensions: ['.mjs', '.js', '.jsx', '.json', '.node']
+        extensions: ['.mjs', '.js', '.jsx', '.json', '.node', '.tsx', '.ts']
       }),
       commonjs({
         include: /\/node_modules\//,
         esmExternals: false,
         requireReturnsDefault: 'namespace',
+        ignoreGlobal: true,
       }),
+      nodeGlobals(),
       typescript(),
       image(),
       json(),
